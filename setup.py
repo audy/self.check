ddd0eb82c43cfb7117610ac033f8c2527f80e5c7
Austin Davis-Richardson <harekrishna@gmail.com>
2018-01-10 21:43:55 -0800

    package beginnings

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='self.check',
+    version='0.0.1',
+    description='Keep track of self-reported data about yourself',
+    url='http://github.com/audy/self.check',
+    author='Austin Davis-Richardson',
+    author_email='harekrishna@gmail.com',
+    zip_safe=True
+)
