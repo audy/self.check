b9f5af5f87ba9432d4391c9ee6f3f4103821450a
Austin Davis-Richardson <harekrishna@gmail.com>
2018-01-16 15:40:11 -0800

    + questions.

---
+++ questions.example.py
@@ -0,0 +1,12 @@
+questions = [
+    {
+        'id': 'cats',
+        'prompt': 'how many cats did you pet today?',
+        'type': int
+    },
+    {
+        'id': 'random-thought',
+        'prompt': 'write down a random thought:',
+        'type': str
+    },
+]
